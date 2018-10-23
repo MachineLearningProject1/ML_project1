a637a7983811911992d94d68f528602789d3794d
s171992 <44138655+s171992@users.noreply.github.com>
2018-10-24 01:02:24 +0800

    add correlation select

--- project_main.py
+++ project_main.py
@@ -4,9 +4,9 @@ from project_func import *
 from preprocessing import replace_invalid
 
 ## load the data
-path = '../data/train.csv'
+path = 'train.csv'
 label_train, input_train, ids = load_csv_data(path, sub_sample=False)
-path = '../data/test.csv'
+path = 'test.csv'
 label_test, input_test, ids_test = load_csv_data(path, sub_sample=False)
 
 input_train_reduced = input_train[:,[1,2,6,11,12,24]]
@@ -17,10 +17,30 @@ value_invalid = -999
 x_valid = replace_invalid_crude(input_train,value_invalid)
 x_test_valid = replace_invalid_crude(input_test,value_invalid)
 
+##calculate correlation
+correlation=np.zeros((30,30))
+ar_n=[]
+acc_s=np.zeros(30)
+for i in range(30):
+    smallnumber=0
+    for x in range(30):
+       corre=np.corrcoef(x_valid[:,i],x_valid[:,x])
+       correlation[i,x]=abs(corre[0,1])
+       if correlation[i,x]<0.2: #account the time that correlation<0.2
+           smallnumber=smallnumber+1
+    if smallnumber>25:
+        ar_n=np.append(ar_n,i)
+    acc_s[i]=smallnumber  
+#delete the line which is super low correlation(less than 0.2 and the time more than 25) with the others
+ar_n=ar_n[::-1]
+for i in range(np.size(ar_n)):
+    x_test_valid=np.delete(x_test_valid,ar_n[i],1)
+    x_valid=np.delete(x_valid,ar_n[i],1)
+    
 ## logistic regression and prediction
 alpha = 0.01
 max_iters = 600
-poly_degree = 3
+poly_degree = 3 #2 and 3 are the best
 x_std = standardize(x_valid)
 tx = build_poly(x_std, poly_degree)
 y = label_train
@@ -34,7 +54,6 @@ u_estimated[u_estimated < 0.5] = 0.
 y_estimated = 2.0*u_estimated - 1
 correct_rate = np.sum(np.abs(y_estimated - y))/2.0
 correct_rate = 1 - correct_rate/y.shape
-
 ## Cross validation
 
 ## Prediction
@@ -45,7 +64,5 @@ u_pre[u_pre >= 0.5] = 1.
 u_pre[u_pre < 0.5] = 0.
 y_pre = 2.0*(u_pre)-1
 
-
 ## generate the submission 
-create_csv_submission(ids_test, y_pre, 'Submission_2210_3.csv')
-
+create_csv_submission(ids_test, y_pre, 'Submission_2210_7.csv')
